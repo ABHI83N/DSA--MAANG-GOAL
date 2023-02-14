b99095d35b40e1aa0fdd633a488c19d8d5e37cc7
Abhishek Shukla <100490265+ABHI83N@users.noreply.github.com>
2023-02-14 20:33:56 +0530

    Update Pangram.cpp

--- STRINGS/LEET CODE 1832/Pangram.cpp
+++ STRINGS/LEET CODE 1832/Pangram.cpp
@@ -1,18 +1,25 @@
-class Solution {
-public:
-    bool checkIfPangram(string sentence) {
-        vector<int> arr(26,0);
-        int count=0;
-
-        for(char &ch: sentence){
-            int index = ch -'a';
-                arr[index]++; }
-        for(int &count :arr){
-            if(count==0)
-            return false;
-        }
-         return true;
-       
-        
+class solution{
+  public:
+    bool Checkifpangram(string sentence){
+      // Create vector arr with index of 26 and value in it is 0 .
+      vector<int> arr(26,0);
+      
+      int count=0;
+      // Traverse the sentence and subract each alphabate with 'a' and store in index;
+      
+      for(char &ch : sentence){
+        int index = ch - 'a';
+        // After subraction if index value is '0' then increment index and count by 1;
+         if(arr[index]==0){
+           arr[index]++;
+           count++;
+         }
+      }
+       // Check if count value is 26 then return true othewise return false
+      if(count==26)
+        return true;
+      return false;
     }
-};//TIME COMPLEXITY IS O(N)....
+};
+ 
+// TIME COMPLEXCITIY OF THIS CODE IS O(n).....
